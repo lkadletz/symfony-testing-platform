378012cf2bd2b89c4a718a134f015a69b436d04e
Lukas Kadletz <lukas@kadletz.cz>
2021-02-07 22:15:49 +0100

    env hack

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,23 +3,48 @@
 
 namespace App\Twig;
 
+use http\Env;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension {
 
     public function getFunctions() : array{
+
+        // in test mode load without runtime
+        if($_ENV['APP_DEBUG'] === '1') {
+            return [
+                new TwigFunction('tableBuilder',
+                    [new TableBuilderExtension(), 'tableBuilder'],
+                    [
+                        'needs_environment' => true,
+                        'is_safe' => ['html']
+                    ]),
+                new TwigFunction('one',
+                    [new TableBuilderExtension(), 'one']
+                ),
+                new TwigFunction('one2',
+                    [$this, 'one2']),
+            ];
+        } else {
+            return [
+                new TwigFunction('tableBuilder',
+                    [TableBuilderExtension::class, 'tableBuilder'],
+                    [
+                        'needs_environment' => true,
+                        'is_safe' => ['html']
+                    ]),
+                new TwigFunction('one',
+                    [TableBuilderExtension::class, 'one']
+                ),
+                new TwigFunction('one2',
+                    [$this, 'one2']),
+            ];
+        }
+
+
         return [
-            new TwigFunction('tableBuilder',
-                [TableBuilderExtension::class, 'tableBuilder'],
-                [
-                    'needs_environment' => true,
-                    'is_safe' => ['html']
-                ]),
-            new TwigFunction('one',
-                [TableBuilderExtension::class, 'one']),
-            new TwigFunction('one2',
-                [$this, 'one2']),
+
         ];
     }
 
--- tests/tableBuilder/TableBuilderTest.php
+++ tests/tableBuilder/TableBuilderTest.php
@@ -4,12 +4,15 @@
 namespace App\Tests\tableBuilder;
 
 
+use App\Kernel;
 use App\Twig\AppExtension;
+use App\Twig\TableBuilderExtension;
 use Twig\Test\IntegrationTestCase;
 
 class TableBuilderTest extends IntegrationTestCase {
 
     public function getExtensions() : array {
+
         return [
             new AppExtension()
         ];
