1706200d63e6b7b6af328f3dd7b6d44aafdc6dec
Lukas Kadletz <lukas@kadletz.cz>
2021-02-12 16:05:25 +0100

    test html table

--- tests/TableHtmlTest.php
+++ tests/TableHtmlTest.php
@@ -6,6 +6,7 @@ use App\TableBuilder\Table;
 use App\Twig\TableBuilderExtension;
 use PHPStan\DependencyInjection\LoaderFactory;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DomCrawler\Crawler;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
@@ -15,20 +16,23 @@ class TableHtmlTest extends TestCase
 
     public function setUp(): void {
         $loader = new FilesystemLoader('templates');
-        $this->twigEnv = new Environment($loader, [
-            'cache' => 'var/cache',
-        ]);
+        $this->twigEnv = new Environment($loader);
     }
 
-    public function testTableGenerator(): void
+    public function testTableCaption(): void
     {
         $object = new TableBuilderExtension();
 
-        $table = new Table('Table');
+        $table = new Table('ABcd');
         $table->addColumn('one');
 
         $html = $object->tableBuilder($this->twigEnv, $table);
 
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($html);
+
+        $this->assertSame('ABcd', $crawler?->filterXPath('//caption')?->getNode(0)?->nodeValue,
+            'Tag <caption> not found :(');
         $this->assertStringContainsString('<table>', $html);
         $this->assertTrue(true);
     }
