9d5624e1d77be2ee84ed0ac3ce83e83886d6a91c
Lukas Kadletz <lukas@kadletz.cz>
2021-02-12 11:32:50 +0100

    form ok, table ok

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,40 +4,68 @@ namespace App\Controller;
 
 use App\TableBuilder\Table;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController {
 
     #[Route(path: '/', name: 'homepage')]
-    public function homepage(): Response {
+    public function homepage(Request $request): Response {
+
+        $form = $this->createFormBuilder();
+        $form->add('rows', IntegerType::class, [
+            'attr' => ['min' => 1, 'max' => 999]
+        ]);
+        $form->add('cols', IntegerType::class, [
+            'attr' => ['min' => 1, 'max' => 99]
+        ]);
+        $form->add('odeslat', SubmitType::class, ['label' => 'Odeslat']);
+
+        $form = $form->getForm();
+
+        $form->handleRequest($request);
+
+        $rows = 1;
+        $cols = 1;
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $rows = (int) $data['rows'];
+            $cols = (int) $data['cols'];
+        }
 
         $table = new Table('Table');
 
         // cols
-        for($i = 0; $i < 20; $i++) {
+        for($i = 0; $i < $cols; $i++) {
             $table->addColumn('col' . $i);
         }
 
         // rows
-        for($i = 0; $i < 300; $i++) {
+        for($i = 0; $i < $rows; $i++) {
             $row = $table->addRow(1);
-            for($j = 0; $j < 20; $j++) {
+            for($j = 0; $j < $cols; $j++) {
                 $row->addCell('cell-' . $i . '-' . $j);
             }
         }
 
         // footer
-        for($i = 0; $i < 10; $i++) {
+        for($i = 0; $i < 1; $i++) {
             $row = $table->addFooterRow();
-            for($j = 0; $j < 20; $j++) {
+            for($j = 0; $j < $cols; $j++) {
                 $row->addCell('foot-' . $i . '-' . $j);
             }
         }
 
         return $this->render('homepage/homepage.html.twig', [
             'content' => true,
-            'table' => $table
+            'table' => $table,
+            'form' => $form->createView()
         ]);
     }
 
